f9c6b8859e77bd4e97995fc175236865ae086b99
pvhiep96@gmail.com <pvhiep96@gmail.com>
2025-09-16 21:16:41 +0700

    Disable CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,25 @@
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins 'http://localhost:3001', 'http://localhost:3000', 'http://127.0.0.1:3001', 'http://127.0.0.1:3000', 'http://47.129.168.239', 'https://47.129.168.239',
+#               'http://47.129.168.239:80', 'http://47.129.168.239:3000',
+#               'https://47.129.168.239:80', 'https://47.129.168.239:3000'
+
+#     resource '*',
+#       headers: :any,
+#       methods: [:get, :post, :put, :patch, :delete, :options, :head],
+#       credentials: true,
+#       expose: ['access-token', 'expiry', 'token-type', 'Authorization']
+#   end
+# end
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001', 'http://localhost:3000', 'http://127.0.0.1:3001', 'http://127.0.0.1:3000', 'http://47.129.168.239', 'https://47.129.168.239',
-              'http://47.129.168.239:80', 'http://47.129.168.239:3000',
-              'https://47.129.168.239:80', 'https://47.129.168.239:3000'
+    origins '*'  # Allow all origins
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true,
+      credentials: false,  # Must be false when using wildcard
       expose: ['access-token', 'expiry', 'token-type', 'Authorization']
   end
 end 
