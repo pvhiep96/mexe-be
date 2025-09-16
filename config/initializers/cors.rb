44b57e956aa67e7b5eb3e46b05f3ff7c9d10a5d4
pvhiep96@gmail.com <pvhiep96@gmail.com>
2025-09-16 21:15:00 +0700

    fix CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001', 'http://localhost:3000', 'http://127.0.0.1:3001', 'http://127.0.0.1:3000'
+    origins 'http://localhost:3001', 'http://localhost:3000', 'http://127.0.0.1:3001', 'http://127.0.0.1:3000', 'http://47.129.168.239', 'https://47.129.168.239',
+              'http://47.129.168.239:80', 'http://47.129.168.239:3000',
+              'https://47.129.168.239:80', 'https://47.129.168.239:3000'
 
     resource '*',
       headers: :any,
